b0c7deb1981fffdd6ff36f143267a3d4ed665c17
muxacb1 <64861213+muxacb1@users.noreply.github.com>
2021-01-13 09:44:21 +0300

    1

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,9 @@
+//Tippy
+tippy ('.tippy', {
+  content:"Наша компания обладает современными технологиями, которые позволяют купить/продать недвижимость в любой точке мира в кратчайшие сроки.",
+});
+
+//Swiper
 var mySwiper = new Swiper('.swiper-container', {
     autoHeight: true,
     spaceBetween: 41,
@@ -35,47 +41,16 @@ var mySwiper = new Swiper('.swiper-container', {
 
 })
 
-//---------additional
 
-class Tabs{
-  constructor(){
-    this.tabList = document.querySelectorAll('.pageNav__tabItem');
-    this.contentList = document.querySelectorAll('.pageNav__contentItem');
-    let nav = document.querySelector('.pageNav');
-    
-    nav.addEventListener('click', e => this.show(e));
-    
-    this.setIndex();
-  }
-  
-  show(e){
-    let t = e.target;
-    if (!t.classList.contains('pageNav__tabItem')) return;
-    this.removePrev();
-    
-    let index = t.getAttribute('data-index');
-    let content = document.querySelector('.pageNav__contentItem[data-index="'+index+'"]');
-    
-    t.classList.add('pageNav__tabItem--active');
-    content.classList.add('pageNav__contentItem--active');
-  }
-  
-  setIndex(){
-    for (let i = 0; i < this.tabList.length; i++){
-      this.tabList[i].setAttribute('data-index', i);
-      this.contentList[i].setAttribute('data-index', i);
-    }
-  }
-  
-  removePrev(){
-    for (let i = 0; i < this.tabList.length; i++){
-      this.tabList[i].classList.remove('pageNav__tabItem--active');
-      this.contentList[i].classList.remove('pageNav__contentItem--active');
-    }
-  }
-  
-}
+//additional
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  let tabs = new Tabs();
-})
+$('.js-tab-trigger').click(function() {
+   var id = $(this).attr('data-tab'),
+       content = $('.js-tab-content[data-tab="'+ id +'"]');
+   
+   $('.js-tab-trigger.active').removeClass('active');
+   $(this).addClass('active');
+   
+   $('.js-tab-content.active').removeClass('active');
+   content.addClass('active');
+});
