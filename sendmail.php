d4e8c470a65aaa21bffcaeee2afcdc09dbadfe15
muxacb1 <64861213+muxacb1@users.noreply.github.com>
2021-01-30 14:15:13 +0300

    1

--- sendmail.php
+++ sendmail.php
@@ -19,34 +19,17 @@
 
 
 	//Тело письма
-	$body = '<h1>Встречайте супер письмо!</h1>';
+	$body = '<h1>Заявка с сайта!</h1>';
 	
 	if(trim(!empty($_POST['name']))){
 		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
 	}
-	if(trim(!empty($_POST['tel-m']))){
-		$body.='<p><strong>Телефон:</strong> '.$_POST['tel-m'].'</p>';
-	}
-	if(trim(!empty($_POST['tel-b']))){
-		$body.='<p><strong>Телефон:</strong> '.$_POST['tel-b'].'</p>';
-	}
 	if(trim(!empty($_POST['tel']))){
 		$body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>';
-	}
 	
-	//Прикрепить файл
-	if (!empty($_FILES['image']['tmp_name'])) {
-		//путь загрузки файла
-		$filePath = __DIR__ . "/files/" . $_FILES['image']['name']; 
-		//грузим файл
-		if (copy($_FILES['image']['tmp_name'], $filePath)){
-			$fileAttach = $filePath;
-			$body.='<p><strong>Фото в приложении</strong>';
-			$mail->addAttachment($fileAttach);
-		}
-	}
 
-	$mail->Body = $body;
+
+//	$mail->Body = $body;
 
 	//Отправляем
 	if (!$mail->send()) {
