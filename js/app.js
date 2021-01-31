6e4e79ca2411cd90f6b215cfbc24556eb3e55ae9
muxacb1 <64861213+muxacb1@users.noreply.github.com>
2021-01-31 11:04:53 +0300

    1

--- js/app.js
+++ js/app.js
@@ -564,156 +564,152 @@ async function form_submit(e) {
 		const message = form.getAttribute('data-message');
 		const ajax = form.getAttribute('data-ajax');
 
-		//SendForm
-		if (ajax) {
-			e.preventDefault();
-			let formData = new FormData(form);
-			form.classList.add('_sending');
-			let response = await fetch(formAction, {
-				method: formMethod,
-				body: formData
-			});
-			if (response.ok) {
-				let result = await response.json();
-				form.classList.remove('_sending');
-				if (message) {
-					popup_open('_' + message + '-message');
-				}
-				form_clean(form);
-			} else {
-				alert("Ошибка");
-				form.classList.remove('_sending');
-			}
-		}
-	} else {
-		let form_error = form.querySelectorAll('._error');
-		if (form_error && form.classList.contains('_goto-error')) {
-			_goto(form_error[0], 1000, 50);
-		}
-		e.preventDefault();
-	}
+      //SendForm
+      if (ajax) {
+         e.preventDefault();
+         let formData = new FormData(form);
+         let response = await fetch(formAction, {
+            method: formMethod,
+            body: formData
+         });
+         if (response.ok) {
+            let result = await response.json();
+            if (message) {
+               popup_open('_' + message + '-message');
+            }
+            form_clean(form);
+         } else {
+         }
+      }
+   } else {
+      let form_error = form.querySelectorAll('._error');
+      if (form_error && form.classList.contains('_goto-error')) {
+         _goto(form_error[0], 1000, 50);
+      }
+      e.preventDefault();
+   }
 }
 function form_validate(form) {
-	let error = 0;
-	let form_req = form.querySelectorAll('._req');
-	if (form_req.length > 0) {
-		for (let index = 0; index < form_req.length; index++) {
-			const el = form_req[index];
-			if (!_is_hidden(el)) {
-				error += form_validate_input(el);
-			}
-		}
-	}
-	return error;
+   let error = 0;
+   let form_req = form.querySelectorAll('._req');
+   if (form_req.length > 0) {
+      for (let index = 0; index < form_req.length; index++) {
+         const el = form_req[index];
+         if (!_is_hidden(el)) {
+            error += form_validate_input(el);
+         }
+      }
+   }
+   return error;
 }
 function form_validate_input(input) {
-	let error = 0;
-	let input_g_value = input.getAttribute('data-value');
+   let error = 0;
+   let input_g_value = input.getAttribute('data-value');
 
-	if (input.getAttribute("name") == "email" || input.classList.contains("_email")) {
-		if (input.value != input_g_value) {
-			let em = input.value.replace(" ", "");
-			input.value = em;
-		}
-		if (email_test(input) || input.value == input_g_value) {
-			form_add_error(input);
-			error++;
-		} else {
-			form_remove_error(input);
-		}
-	} else if (input.getAttribute("type") == "checkbox" && input.checked == false) {
-		form_add_error(input);
-		error++;
-	} else {
-		if (input.value == '' || input.value == input_g_value) {
-			form_add_error(input);
-			error++;
-		} else {
-			form_remove_error(input);
-		}
-	}
-	return error;
+   if (input.getAttribute("name") == "email" || input.classList.contains("_email")) {
+      if (input.value != input_g_value) {
+         let em = input.value.replace(" ", "");
+         input.value = em;
+      }
+      if (email_test(input) || input.value == input_g_value) {
+         form_add_error(input);
+         error++;
+      } else {
+         form_remove_error(input);
+      }
+   } else if (input.getAttribute("type") == "checkbox" && input.checked == false) {
+      form_add_error(input);
+      error++;
+   } else {
+      if (input.value == '' || input.value == input_g_value) {
+         form_add_error(input);
+         error++;
+      } else {
+         form_remove_error(input);
+      }
+   }
+   return error;
 }
 function form_add_sent(input) {
-	input.classList.add('_sent');
-	input.parentElement.classList.add('_sent');
+   input.classList.add('_sent');
+   input.parentElement.classList.add('_sent');
 
-	let input_error = input.parentElement.querySelector('.form__sent');
-	if (input_error) {
-		input.parentElement.removeChild(input_error);
-	}
-	
-	input.parentElement.insertAdjacentHTML('beforeend', '<div class="form__sent">Ваше письмо отправлено</div>');
-	
+   let input_error = input.parentElement.querySelector('.form__sent');
+   if (input_error) {
+      input.parentElement.removeChild(input_error);
+   }
+   
+   input.parentElement.insertAdjacentHTML('beforeend', '<div class="form__sent">Ваше письмо отправлено</div>');
+   
 }
 function form_remove_sent(input) {
-	input.classList.remove('_sent');
-	input.parentElement.classList.remove('_sent');
+   input.classList.remove('_sent');
+   input.parentElement.classList.remove('_sent');
 
-	let input_error = input.parentElement.querySelector('.form__error');
-	if (input_error) {
-		input.parentElement.removeChild(input_error);
-	}
+   let input_error = input.parentElement.querySelector('.form__error');
+   if (input_error) {
+      input.parentElement.removeChild(input_error);
+   }
 }
 function form_add_error(input) {
-	input.classList.add('_error');
-	input.parentElement.classList.add('_error');
+   input.classList.add('_error');
+   input.parentElement.classList.add('_error');
 
-	let input_error = input.parentElement.querySelector('.form__error');
-	if (input_error) {
-		input.parentElement.removeChild(input_error);
-	}
-	let input_error_text = input.getAttribute('data-error');
-	if (input_error_text && input_error_text != '') {
-		input.parentElement.insertAdjacentHTML('beforeend', '<div class="form__error">' + input_error_text + '</div>');
-	}
+   let input_error = input.parentElement.querySelector('.form__error');
+   if (input_error) {
+      input.parentElement.removeChild(input_error);
+   }
+   let input_error_text = input.getAttribute('data-error');
+   if (input_error_text && input_error_text != '') {
+      input.parentElement.insertAdjacentHTML('beforeend', '<div class="form__error">' + input_error_text + '</div>');
+   }
 }
 function form_remove_error(input) {
-	input.classList.remove('_error');
-	input.parentElement.classList.remove('_error');
+   input.classList.remove('_error');
+   input.parentElement.classList.remove('_error');
 
-	let input_error = input.parentElement.querySelector('.form__error');
-	if (input_error) {
-		input.parentElement.removeChild(input_error);
-	}
+   let input_error = input.parentElement.querySelector('.form__error');
+   if (input_error) {
+      input.parentElement.removeChild(input_error);
+   }
 }
 function form_clean(form) {
-	let inputs = form.querySelectorAll('input,textarea');
-	for (let index = 0; index < inputs.length; index++) {
-		const el = inputs[index];
-		el.parentElement.classList.remove('_focus');
-		el.classList.remove('_focus');
-		el.value = el.getAttribute('data-value');
-	}
-	let checkboxes = form.querySelectorAll('.checkbox__input');
-	if (checkboxes.length > 0) {
-		for (let index = 0; index < checkboxes.length; index++) {
-			const checkbox = checkboxes[index];
-			checkbox.checked = false;
-		}
-	}
-	let selects = form.querySelectorAll('select');
-	if (selects.length > 0) {
-		for (let index = 0; index < selects.length; index++) {
-			const select = selects[index];
-			const select_default_value = select.getAttribute('data-default');
-			select.value = select_default_value;
-			select_item(select);
-		}
-	}
+   let inputs = form.querySelectorAll('input,textarea');
+   for (let index = 0; index < inputs.length; index++) {
+      const el = inputs[index];
+      el.parentElement.classList.remove('_focus');
+      el.classList.remove('_focus');
+      el.value = el.getAttribute('data-value');
+   }
+   let checkboxes = form.querySelectorAll('.checkbox__input');
+   if (checkboxes.length > 0) {
+      for (let index = 0; index < checkboxes.length; index++) {
+         const checkbox = checkboxes[index];
+         checkbox.checked = false;
+      }
+   }
+   let selects = form.querySelectorAll('select');
+   if (selects.length > 0) {
+      for (let index = 0; index < selects.length; index++) {
+         const select = selects[index];
+         const select_default_value = select.getAttribute('data-default');
+         select.value = select_default_value;
+         select_item(select);
+      }
+   }
 }
 
 let viewPass = document.querySelectorAll('.form__viewpass');
 for (let index = 0; index < viewPass.length; index++) {
-	const element = viewPass[index];
-	element.addEventListener("click", function (e) {
-		if (element.classList.contains('_active')) {
-			element.parentElement.querySelector('input').setAttribute("type", "password");
-		} else {
-			element.parentElement.querySelector('input').setAttribute("type", "text");
-		}
-		element.classList.toggle('_active');
-	});
+   const element = viewPass[index];
+   element.addEventListener("click", function (e) {
+      if (element.classList.contains('_active')) {
+         element.parentElement.querySelector('input').setAttribute("type", "password");
+      } else {
+         element.parentElement.querySelector('input').setAttribute("type", "text");
+      }
+      element.classList.toggle('_active');
+   });
 }
 
 
--- js/custom.js
+++ js/custom.js
@@ -3,97 +3,96 @@
 let form;
 
 document.addEventListener('DOMContentLoaded', function () {
-	form = document.getElementById('form');
-	form.addEventListener('submit', formSend);
+   form = document.getElementById('form');
+   form.addEventListener('submit', formSend);
 
-	form = document.getElementById('form1');
-	form.addEventListener('submit', formSend);
+   form = document.getElementById('form1');
+   form.addEventListener('submit', formSend);
 
-	form = document.getElementById('form2');
-	form.addEventListener('submit', formSend);
+   form = document.getElementById('form2');
+   form.addEventListener('submit', formSend);
 });
 
 
-	async function formSend(e) {
-		e.preventDefault();
-		
+   async function formSend(e) {
+      e.preventDefault();
+      
       let form = e.target;
-		let error = formValidate(form);
+      let error = formValidate(form);
 
-		let formData = new FormData(form);
-		
+      let formData = new FormData(form);
+      
 
-		if (error === 0) {
-			form.classList.add('_sending');
-			let response = await fetch('sendmail.php', {
-				method: 'POST',
-				body: formData
-			});
-			if (response.ok) {
-				let result = await response.json();
-				alert(result.message);
-				formPreview.innerHTML = '';
-				form.reset();
-				let button = document.getElementById('form__btn');
-				form_add_sent(button);
-			} else {
-				//alert("Ошибка");
-				let button = form.submit;
-				form_add_sent(button);
-			}
-		} else {
-			
-		}
+      if (error === 0) {
+         form.classList.add('_sending');
+         let response = await fetch('sendmail.php', {
+            method: 'POST',
+            body: formData
+         });
+         if (response.ok) {
+            let result = await response.json();
+            //alert(result.message);
+            
+            //form.reset();
+            let button = form.submit;
+            form_add_sent(button);
+         } else {
+            //alert("Ошибка");
 
-	}
+         }
+      } else {
+         
+      }
+
+   }
 
 
-	function formValidate(form) {
-		let error = 0;
-		let formReq = document.querySelectorAll('._req');
+   function formValidate(form) {
+      let error = 0;
+      let formReq = form.querySelectorAll('._req');
 
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);
+      for (let index = 0; index < formReq.length; index++) {
+         const input = formReq[index];
+         formRemoveError(input);
 
-			if (input.classList.contains('_email')) {
-				if (emailTest(input)) {
-					form_add_error(input);
-					error++;
-				}
-			} else if (input.classList.contains('_phone')) {
-				let valid = ValidPhone(form.tel.value);
-				if (valid != '') { 
-					form_add_error(input);
-				   error++;
-				}
-			} else {
-				if (input.value === '') {
-					form_add_error(input);
-					error++;
-				}
-			}
-		}
-		return error;
-	}
+         if (input.classList.contains('_email')) {
+            if (emailTest(input)) {
+               form_add_error(input);
+               error++;
+            }
+         } else if (input.classList.contains('_phone')) {
+            let valid = ValidPhone(form.tel.value);
+            if (valid != '') { 
+               form_add_error(input);
+               error++;
+            }
+         } else {
+            if (input.value === '') {
+               form_add_error(input);
+               error++;
+            }
+         }
+      }
+      return error;
+   }
 
 
 function ValidPhone(tel) {
     var re = /(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}/;
     var valid = re.test(tel);
     if (!valid) return 'Номер телефона введен неправильно!'
-    	 else return '';
+       else return '';
 } 
 
 
 
-	function formAddError(input) {
-		input.parentElement.classList.add('form__error');
-		input.classList.add('form__error');
-	}
+   function formAddError(input) {
+      input.parentElement.classList.add('form__error');
+      input.classList.add('form__error');
+   }
 
-	
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('form__error');
-		input.classList.remove('form__error');
-	}
+   
+   function formRemoveError(input) {
+      input.parentElement.classList.remove('form__error');
+      input.classList.remove('form__error');
+   }
